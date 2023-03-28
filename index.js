05c3d43d7c3e6f292ef7336afbf7113e3f7f2077
Jonathan Fajardo <joniten.-@hotmail.com>
2023-03-28 16:32:37 -0300

    arreglamos formulario

--- index.js
+++ index.js
@@ -52,26 +52,3 @@ localStorage.setItem("productos carrito", JSON.stringify(carrito));
 };
 
 JSON.parse(localStorage.getItem("productos carrito"));
-
-// Formulario Email
-
-const btn = document.getElementById('button');
-
-document.getElementById('form')
-.addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    btn.value = 'Sending...';
-
-    const serviceID = 'default_service';
-    const templateID = 'template_mg8h7fe';
-
-    emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
-        btn.value = 'Send Email';
-        alert('Sent!');
-    }, (err) => {
-        btn.value = 'Send Email';
-        alert(JSON.stringify(err));
-    });
-});
